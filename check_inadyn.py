c8c9390ad3c9fb128034b434d1576228c9586aa7
Zer0C001 <georgi.saev@gmail.com>
2014-03-10 13:28:56 +0200

    added hostname check

--- check_inadyn.py
+++ check_inadyn.py
@@ -1,25 +1,50 @@
 #!/usr/bin/python
 import sys
+import socket
 f=open('/var/run/inadyn/inadyn.cache','r')
 inadyn_ip=f.read()
 f.close()
-ips={'k':(),'w':()}
+ips={'k':(),'w':(),'h':()}
+host_ip=''
 nextarg=''
+exit_st=0
+host_msg=''
 for arg in sys.argv:
-	if nextarg!='' and arg!='':
+	if nextarg!='' and arg!='' and arg!='-k' and arg!='-w' and arg!='-h':
 		ips.update({nextarg:arg.split(',')})
-	if arg=='-k':
+	elif arg=='-k':
 		nextarg='k'
 	elif arg=='-w':
 		nextarg='w'
+	elif arg=='-h':
+		nextarg='h'
 	else:
 		nextarg=''	
+if len(ips['h'])>1:
+	host_msg='warn: not resolving multiple hostnames'
+	exit_st=1
+elif len(ips['h'])==1:
+	host_ip=socket.gethostbyname(ips['h'][0])
+	if host_ip==inadyn_ip:
+		host_msg='hostname: ok'
+	else:
+		host_msg='hostname( '+ips['h'][0]+' ): wrong ip: '+host_ip
+		exit_st=1
 if inadyn_ip in ips['k']:
-	print 'ok: current ip: '+inadyn_ip
-	exit(0)
+	if host_msg=='':
+		print 'ok: current ip: '+inadyn_ip
+	else:
+		print 'ok: current ip: '+inadyn_ip+' ; '+host_msg
+	exit(exit_st)
 elif (inadyn_ip in ips['w']) or ips['w']==():
-	print 'warn: current ip: '+inadyn_ip
+	if host_msg=='':
+		print 'warn: current ip: '+inadyn_ip
+	else:
+		print 'warn: current ip: '+inadyn_ip+' ; '+host_msg
 	exit(1)
 else:
-	print 'err: current ip: '+inadyn_ip
+	if host_msg=='':
+		print 'err: current ip: '+inadyn_ip
+	else:
+		print 'err: current ip: '+inadyn_ip+' ; '+host_msg
 	exit(2)
